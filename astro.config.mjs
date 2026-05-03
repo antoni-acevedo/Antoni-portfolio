94f9ff2ca74fcbb05d222d84d7d621b328d12616
Antoni Acevedo <programador91@solucionesstar.com>
2026-05-03 17:50:18 -0500

    feat: update site URL, base path, and output settings in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nadxus.github.io', // Nombre del repositorio (case-sensitive)
-  base: '/Portfolio/', // Agregamos slash final para evitar errores de concatenación
-  output: 'server',
+  site: 'https://antoni-acevedo.github.io',
+  base: '/Portfolio',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [react()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
 });
