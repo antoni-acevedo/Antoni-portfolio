b101884e027f86c46cc5ba879a64e5352f262ab3
Antoni Acevedo <programador91@solucionesstar.com>
2026-05-03 20:34:14 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://antoni-acevedo.github.io',
-  base: '/Portfolio/',
+  base: '/Antoni-portfolio/',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
